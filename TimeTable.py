da0ba36531854b0b5c7fc75b2918f8c4b4b70700
chandu <59366291+chandu0820@users.noreply.github.com>
2020-05-14 14:05:58 +0530

    Two Sections

---
+++ TimeTable.py
@@ -0,0 +1,214 @@
+import random
+m=6
+n=8
+Periods=m*n
+print("Enter no.of Subjects,no.of default_periods,no.of labs,no.of free_periods::")
+sub,dp,l,fp=map(int,input().split())
+
+
+emp=""
+p=[[emp for i in range(n+5)]for j in range(m+5)] #sec1
+q=[[emp for i in range(n+5)]for j in range(m+5)] #sec2
+freq=[]
+freq1=[]
+
+default=[]
+print("Enter",dp,"default_periods...")
+for i in range(0,dp):
+    default.append(input())
+print()
+
+fperiods=[]
+print("Enter",fp,"free_periods...")
+for i in range(0,fp):
+    fperiods.append(input())
+print()
+
+labs=[]
+labs_faculty=[]
+lab_details={}
+print("Enter",l,"lab_periods...")
+for i in range(0,l):
+    print("enter lab",i,":",end=" ")
+    labs.append(input())
+    #print("enter faculty for ",labs[i],"lab:",end=" ")
+    #labs_faculty.append(input())
+    #lab_details[labs_faculty[i]]=labs[i]
+print()
+
+main_subjects=[]
+sub_faculty=[]
+subject_faculty={}
+print("Enter",sub,"Subjects...")
+for i in range(0,sub):
+    print("enter subject", i,":",end=" ")
+    main_subjects.append(input())
+    #print("enter faculty for ",main_subjects[i],":",end=" ")
+    #sub_faculty.append(input())
+    #subject_faculty[sub_faculty[i]]=main_subjects[i]
+print()
+
+#Section1 Allocation starts from here.....
+def sec1():
+    def default_periods():
+        def exams():
+                i=2
+                for j in range(0,i):
+                        p[i][j]=default[0] #allocating exams for Section1
+                        q[i][j]=default[0] #allocating exams for Section2
+        exams()
+
+        def rev():
+                i=1
+                for j in range(4,n):
+                        p[i][j]=default[1]  #allocating revision for Section1
+                        q[i][j]=default[1]  #allocating revision for Section2
+        rev()
+
+        def elec(): #Elective is also a subject
+            i=2
+            count=0
+            while count<5:
+                k=random.randint(0,5)
+                s=random.randint(0,6)
+                if p[k][s]!=emp or q[k][s]!=emp :
+                  continue
+                else:
+                  p[k][s]=default[i]  #allocating elective for Section1
+                  q[k][s]=default[i]  #allocating elective for Section2
+                  count=count+1
+        elec()
+
+    default_periods()
+
+    def free_periods():
+        def crt():
+          i=0
+          count=0
+          while count<3:
+              k=random.randint(0,5)
+              s=random.randint(0,6)
+              if p[k][s]!=emp:
+                continue
+              else:
+                p[k][s]=fperiods[i]
+                count=count+1
+        crt()
+
+        def free():
+          count=0
+          while count<5:
+              k=random.randint(0,5)
+              s=7
+              if p[k][s]!=emp and q[k][s]!=emp:
+                continue
+              else:
+                p[k][s]=fperiods[1]
+                q[k][s]=fperiods[1]
+                count=count+1
+        free()
+    free_periods()
+
+    def lab_periods(): #Allocating Labs for Section1
+        def lab():
+                i=0
+                count =0
+                while count<9:
+                    k=random.randint(0,5)
+                    s=random.randint(0,4)
+                    if (p[k][s]!=emp or p[k][s+1]!=emp or p[k][s+2]!=emp):
+                        continue
+                    else:
+                        for j in range(0,3):
+                            p[k][s+j]=labs[i]
+                            count=count+i
+                        i+=1
+        lab()
+    lab_periods()
+
+    def subj():
+        def subjects(): #Allocating main_subjects for Section1
+          i=0
+          count=0
+          while count<20:
+            while freq.count(main_subjects[i])<5:
+              k=random.randint(0,5)
+              s=random.randint(0,6)
+              if p[k][s]!=emp:
+                continue
+              else:
+                p[k][s]=main_subjects[i]
+                count=count+1
+                freq.append(main_subjects[i])
+            i=i+1
+        subjects()
+    subj()
+sec1()
+#Section allocation starts from here.....
+def sec2():
+    def crt1():
+      i=0
+      count=0
+      while count<3:
+          k=random.randint(0,5)
+          s=random.randint(0,7)
+          if q[k][s]!=emp or p[k][s]==fperiods[i]:
+            continue
+          else:
+            q[k][s]=fperiods[i]
+            count=count+1
+    crt1()
+
+    def labs1(): #Allocating labs for Section2
+            i=0
+            count=0
+            while count<9:
+                    k=random.randint(0,5)
+                    s=random.randint(0,4)
+                    if (q[k][s]!=emp or q[k][s+1]!=emp or q[k][s+2]!=emp or p[k][s]==labs[i] or p[k][s+1]==labs[i] or p[k][s+2]==labs[i]):
+                            continue
+                    else:
+                            for j in range(0,3):
+                                    q[k][s+j]=labs[i]
+                                    count=count+1
+                            i+=1
+    labs1()
+
+
+
+    def subjects1(): #Allocating main_subjects for Section2
+      i=0
+      count=0
+      while count<20:
+        while freq1.count(main_subjects[i])<5:
+          k=random.randint(0,5)
+          s=random.randint(0,6)
+          if q[k][s]!=emp or p[k][s]==main_subjects[i]:
+            continue
+          else:
+            q[k][s]=main_subjects[i]
+            count=count+1
+            freq1.append(main_subjects[i])
+        i=i+1
+    subjects1()
+sec2()
+
+#printing Section1 TimeTable
+for i in range(0,m):
+    for j in range(0,n):
+        print(p[i][j],end=" | ")
+    print()
+print()
+print()
+#printing Section2 TimeTable
+for i in range(0,m):
+    for j in range(0,n):
+        print(q[i][j],end=" | ")
+    print()
+print()
+print()
+
+
+
+print("subjects:",subject_faculty)
+print("labs:",lab_details)
